c825da4ff021c323497aaac86fdcb6c2e4f7005a
Daiderd Jordan <daiderd@gmail.com>
2012-12-02 11:36:26 +0100

    very basic Display implementation

---
+++ display.rkt
@@ -0,0 +1,51 @@
+#lang racket/base
+(require "lib/graphics.rkt")
+(provide MakeDisplay)
+
+
+
+(define (MakeDisplay width height)
+  (let ((_canvas (make-graphics width height))
+        (_width  width)
+        (_height height))
+    (define (dispatch msg . args)
+      (apply
+        (cond
+          ((eq? msg 'rectangle) shape_rectangle)
+          ((eq? msg 'ellipse)   shape_ellipse)
+          (else
+            (error msg "method missing ~a" dispatch)))
+        args))
+
+    (define (shape_rectangle rectangle color)
+      (let* ((width  (rectangle 'width))
+             (height (rectangle 'height))
+             (pos    (rectangle 'position))
+             (x      (pos 'x))
+             (y      (pos 'y)))
+        (start!
+            (lambda (delta)
+              (use-brush _canvas (to_brush color))
+              (draw-rectangle _canvas x y width height)))))
+
+    (define (shape_ellipse ellipse color)
+      (let* ((width  (ellipse 'width))
+             (height (ellipse 'height))
+             (pos    (ellipse 'position))
+             (x      (pos 'x))
+             (y      (pos 'y)))
+        (start!
+          (lambda (delta)
+            (use-brush _canvas (to_brush color))
+            (draw-ellipse _canvas x y width height)))))
+
+
+    (define (start! event)
+      (chain _canvas animations [add! event])
+      (chain _canvas animations [start]))
+
+    (define (to_brush sym)
+      (let ((str (symbol->string sym)))
+        (new brush% [color str])))
+
+    dispatch))
--- test/shape_render.rkt
+++ test/shape_render.rkt
@@ -1,10 +1,21 @@
 #lang racket/base
 (require "../shape.rkt")
 (require "../console.rkt")
+(require "../display.rkt")
 
 
 
-(let* ((pos (MakePosition 10 10))
-       (vel (MakeVelocity 0 1))
+(define c (MakeConsole))
+(define d (MakeDisplay 300 100))
+
+(let* ((pos       (MakePosition 10 10))
+       (vel       (MakeVelocity 0 1))
        (rectangle (MakeRectangle pos vel 100 50 'red)))
-  (rectangle 'render (MakeConsole)))
+  (rectangle 'render c)
+  (rectangle 'render d))
+
+(let* ((pos     (MakePosition 150 10))
+       (vel     (MakeVelocity 0 1))
+       (ellipse (MakeEllipse pos vel 100 50 'blue)))
+  (ellipse 'render c)
+  (ellipse 'render d))
