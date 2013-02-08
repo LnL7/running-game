9962d7970d27cc1c19e6adfb07372e317a4a6b3a
Daiderd Jordan <daiderd@gmail.com>
2013-02-08 17:03:29 +0100

    separated Display into ScreenEngine and ConsoleEngine

---
+++ screen-engine.rkt
@@ -0,0 +1,55 @@
+#lang racket/base
+(require "logger.rkt"
+         "lib/canvas.rkt"
+         "helpers.rkt")
+(provide MakeScreenEngine
+         (rename-out [kMessages kScreenEngineMessages]))
+
+
+
+(define (MakeScreenEngine #:log [-log (MakeLogger)])
+  (define (dispatch msg . args)
+    (apply
+      (case msg
+        ((rectangle) shape-rectangle)
+        ((ellipse)   shape-ellipse)
+        ((image)     shape-image)
+        (else
+          (-log 'fatal "method missing" msg kClass)))
+      args))
+
+  (define (shape-rectangle rectangle)
+    (let ((width  (rectangle 'width))
+          (height (rectangle 'height))
+          (pos    (rectangle 'position)))
+      (let ((x     (pos 'x))
+            (y     (pos 'y))
+            (color (find-color (rectangle 'color))))
+        (fill-rectangle! x y width height color))))
+
+  (define (shape-ellipse ellipse)
+    (let ((width  (ellipse 'width))
+          (height (ellipse 'height))
+          (pos    (ellipse 'position)))
+      (let ((x     (pos 'x))
+            (y     (pos 'y))
+            (color (find-color (ellipse 'color))))
+        (fill-ellipse! x y width height color))))
+
+  (define (shape-image rectangle)
+    (let ((pos (rectangle 'position)))
+      (let ((x (pos 'x))
+            (y (pos 'y))
+            (image (make-image (rectangle 'path) 'png/alpha)))
+        (draw-image! x y image))))
+
+
+  ;; Private
+
+  (-log 'debug "initialized" kClass)
+
+  dispatch)
+
+
+(define kClass    'ScreenEngine)
+(define kMessages '(rectangle ellipse image))
